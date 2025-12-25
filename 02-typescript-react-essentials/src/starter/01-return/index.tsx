6c4b2aa8c241cd38370b767aa167e94a3b58248c
Steve Saju <stevesajustephen@gmail.com>
2025-12-25 21:53:05 +0100

    ts explicit type annotation

--- 02-typescript-react-essentials/src/starter/01-return/index.tsx
+++ 02-typescript-react-essentials/src/starter/01-return/index.tsx
@@ -2,7 +2,7 @@ function Component(): JSX.Element | null | string {
   return (
     <div>
       <h2>React & Typescript</h2>
-      <h2>Return Type</h2>
+      <h2>Return Typse</h2>
     </div>
   );
 }
