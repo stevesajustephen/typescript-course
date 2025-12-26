8b1d935c72472b687294accda5109f6859e31211
Steve Saju <stevesajustephen@gmail.com>
2025-12-26 12:47:55 +0100

    ts event

--- 02-typescript-react-essentials/src/starter/04-events/index.tsx
+++ 02-typescript-react-essentials/src/starter/04-events/index.tsx
@@ -1,37 +1,64 @@
-import { InputHTMLAttributes, useState } from "react";
+import { useState } from "react";
 
-function Component() {
+interface User {
+  name: string;
+  email: string;
+}
+
+function Component(): JSX.Element {
   const [name, setName] = useState<string>("");
 
   const [email, setEmail] = useState<string>("");
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    console.log(e.target.value);
+    setName(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const newUser: User = {
+      name: formData.get(name) as string,
+      email: formData.get(email) as string,
+    };
+
+    console.log(newUser);
+  };
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Events</h2>
       <section>
-        <form action="" className="form">
+        <form onSubmit={handleSubmit} className="form">
           <div>
             <label htmlFor="name">Name</label>
             <input
-              onChange={(e) => setName(e.target.value)}
+              onChange={handleChange}
               value={name}
               className="form-input mb-1"
               type="text"
-              id="name"
+              name="name"
             />
           </div>
 
           <div>
             <label htmlFor="email">Email</label>
             <input
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
               value={email}
               className="form-input mb-1"
               type="text"
-              id="email"
+              name="email"
             />
           </div>
+
+          <button className="btn btn-block" type="submit">
+            Submit
+          </button>
         </form>
       </section>
     </div>
