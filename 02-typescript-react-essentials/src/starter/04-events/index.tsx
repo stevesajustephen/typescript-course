262e8a84e470a04a4b44a88bb8367c9e7e67f98a
Steve Saju <stevesajustephen@gmail.com>
2025-12-26 11:23:19 +0100

    ts event

--- 02-typescript-react-essentials/src/starter/04-events/index.tsx
+++ 02-typescript-react-essentials/src/starter/04-events/index.tsx
@@ -1,8 +1,39 @@
+import { InputHTMLAttributes, useState } from "react";
+
 function Component() {
+  const [name, setName] = useState<string>("");
+
+  const [email, setEmail] = useState<string>("");
+
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Events</h2>
+      <section>
+        <form action="" className="form">
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="form-input mb-1"
+              type="text"
+              id="name"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="form-input mb-1"
+              type="text"
+              id="email"
+            />
+          </div>
+        </form>
+      </section>
     </div>
   );
 }
