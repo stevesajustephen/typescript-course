50ee60c704318f81f4f52625785f640543fc6140
Steve Saju <stevesajustephen@gmail.com>
2025-12-26 10:13:12 +0100

    ts states

--- 02-typescript-react-essentials/src/starter/03-state/index.tsx
+++ 02-typescript-react-essentials/src/starter/03-state/index.tsx
@@ -1,8 +1,24 @@
-function Component() {
+import { useState } from "react";
+
+function Component(): JSX.Element {
+  const [text, setText] = useState("shakeAndBake");
+
+  const [list, setList] = useState<string[]>([]);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      <button
+        className="btn btn-center"
+        onClick={() => {
+          setText("Clicked!!");
+          setList(["age"]);
+        }}
+      >
+        Click Me
+      </button>
+      <h2>
+        {text} {list[0]}
+      </h2>
     </div>
   );
 }
