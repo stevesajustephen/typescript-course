0d48787cb56c891d7fcf5d7ed943d63b962ddfa2
Steve Saju <stevesajustephen@gmail.com>
2025-12-27 10:50:52 +0100

    challenge ts type annotation

--- 02-typescript-react-essentials/src/starter/05-challenge/index.tsx
+++ 02-typescript-react-essentials/src/starter/05-challenge/index.tsx
@@ -1,8 +1,31 @@
-function Component() {
+type BasicUser = {
+  type: "basic";
+  name: string;
+};
+
+type AdvancedUser = {
+  type: "advanced";
+  name: string;
+  email: string;
+};
+
+type UserProps = BasicUser | AdvancedUser;
+
+function Component(props: UserProps): JSX.Element {
+  const { type, name } = props;
   return (
     <div>
       <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
+      {type === "basic" ? (
+        <>
+          <div>name{name}</div> <div>type{type}</div>{" "}
+        </>
+      ) : (
+        <>
+          <div>name{name}</div>
+          <div>type{type}</div> <div>email{props.email}</div>{" "}
+        </>
+      )}
     </div>
   );
 }
